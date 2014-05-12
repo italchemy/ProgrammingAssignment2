66676e8143a433e868740bd45767d92d770f94ba
Caffeinated Software <caffeinatedsoftware@gmail.com>
2014-05-12 16:00:09 -0500

    Initial commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,79 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains functions to create and manipulate a wrapper for the
+## matrix type that caches its inverse. The purpose of the class is to avoid the
+## computational overhead of re-calculating the matrix inverse once it has
+## already been found.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix - Creates a caching wrapper around the matrix passed into the
+## program arguments.
+## 
+## Parameters:
+## 
+## x - a matrix
+## 
+## Returns: the provided matrix contained within a wrapper that will cache the 
+## matrix inverse once it has been calculated. The returned type is a list with
+## named entries corresponding to the following functions:
+##
+## get - returns the wrapped matrix
+## set - sets the value of the matrix
+## inverse
+##
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <- NULL
+  
+  # Sets the value of the wrapped matrix and stores it in the parent environment
+  
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  # Gets the wrapped matrix
+  
+  get <- function() x
+  
+  # Sets the inverse of the wrapped matrix and stores it in the parent environment
+  
+  setInverse <- function(mInverse) inverse <<- mInverse
+  
+  # Gets the stored inverse
+  
+  getInverse <- function() inverse
+  
+  # List of wrapper functions to enable calling by named element, e.g. x$set(m)
+  
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
-
+## Solves for the inverse of the provided matrix. 
+##
+## Parameters:
+##
+## x - a wrapped matrix previously created by a call to makeCacheMatrix()
+##
+## Returns: the inverse of the provided matrix.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+  inverse <- x$getInverse()
+  
+  if (is.null(inverse)) {
+    
+    # We didn't get a cache hit so solve for the inverse and cache the result
+    
+    m <- x$get()
+    i <- solve(m)
+    x$setInverse(i)
+    inverse <- i
+    
+  }
+  
+  inverse
+  
 }
